7e22c3ec2e0cc1b8224bbcc70fa28ebb8d84ce64
amanmehta-maniac <amanmehta1997@gmail.com>
2018-06-18 12:33:51 +0530

    integration of URIs with annotation done

--- data/integrate.py
+++ data/integrate.py
@@ -2,11 +2,37 @@ import sys
 
 f = open('dbpedia-201610-properties.tsv','r')
 file = f.readlines()
+d = {};
+
+for l in file:
+	l = l.strip().split('\t')
+	d[l[0].split('/')[-1]] = l[1];
+
+# print d;
 
 
-
-
-f = open('manual-annotation-updated','r')
+f = open('manual-annotation.csv','r')
 manual = f.readlines()
+cnt,tot = 0,0;
+final = ""
 
-for 
+for m in manual:
+	l = m.strip().split(',')
+	m = l[0]
+	tot += 1
+	if m in d:
+		cnt += 1;
+		l.append("http://dbpedia.org/ontology/"+m)
+		l.append(d[m])
+	else:
+		l.append('')
+		l.append('')
+		print m
+
+	final += ",".join(l);
+	final += '\n';
+
+print final
+f = open('manual-annotation-updated.csv','w');
+f.write(final);
+print cnt, tot
