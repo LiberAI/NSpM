76ad4f21fcf0cbb2acc2c2769a3e6980d1c90900
amanmehta-maniac <amanmehta1997@gmail.com>
2018-06-25 13:35:49 +0530

    decision tree - version 1 added

--- decision_tree.py
+++ decision_tree.py
@@ -1,1 +1,28 @@
-f = 	
+import sys, re
+
+f = open(sys.argv[1],'r')
+lines = f.readlines();
+
+lineno = 1
+
+# print lines[0].split(',') 
+# ['Property', 'Label ', 'Range', 'Fuzzy Score', 'Comment about expr', 'URI', 'Number of Occurrences', 
+# 'MVE', 'Optimal Expression\r\n']
+
+mve = ""
+for line in lines:
+	if lineno == 1: 
+		lineno += 1
+		continue
+	line = line.strip().split(',')
+	rng = line[2].lower()
+	lbl = line[1]
+	if 'person' in rng:
+		rng = "Who"
+	else:
+		rng = "What"
+	mve += rng + " is the " + lbl + " of <X>\n" 
+
+fw = open('data/mve_output','w')
+fw.write(mve)
+
