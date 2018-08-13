8e85a47710d71dbf624ac92339eac05cc2e0772a
amanmehta-maniac <amanmehta1997@gmail.com>
2018-08-14 02:02:25 +0530

    Entity error detection script added

--- entity_errors.py
+++ entity_errors.py
@@ -2,10 +2,12 @@ import sys
 
 f = open(sys.argv[1],'r')
 g = open(sys.argv[2],'r')
+h = open(sys.argv[3],'r')
 
 
 fl = f.readlines()
 gl = g.readlines()
+hl = h.readlines()
 tot = 0
 entity_error, prop_error = 0, 0
 train_entities = set()
@@ -20,18 +22,30 @@ for i in range(len(fl)):
 # print train_entities
 
 cnt = 0
+save = []
 for i in range(len(gl)):
 	testout = gl[i].strip().split()
 	testEntity = ""
 	for t in testout:
 		if 'dbr_' in t:
 			testEntity = t
-			print testEntity
-			if testEntity in train_entities:
+			if testEntity[1:-1] in train_entities:
+				save.append(i);
 				cnt += 1
 	tot += 1
 
+newtest_nlq, newtest_sparql = [], []
 
-print cnt, tot
+for i in save:
+	newtest_sparql.append(gl[i])
+
+for i in save:
+	newtest_nlq.append(hl[i])
+
+# print "".join(newtest_sparql)
+print "".join(newtest_nlq)
+
+
+# print cnt, tot
 
 
