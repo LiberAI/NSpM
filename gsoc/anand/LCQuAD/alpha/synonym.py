14bca45b34c57e5889a4503d4d7625b9e537ad09
panchbhai1969 <panchbhai1969@gmail.com>
2019-06-17 18:44:00 +0530

    Update 17th May 2019 | gsoc/ folder
    - Aman's pipeline was streamlined and automated
    - All the inconsistencies and the flow works as intended
    - The whole code in gsoc/anand was made python 3 compatible
    - running logs can be found in the data section of working-gsoc-anand branch
    - Full automation of pipeline_3
    - Automatic retreival of URLs and properties built
    - Whole structure of proposed pipeline was completed
    - Recursive quesition SPARQL generation completed
    - Query Check done
    - Answer query done
    - Multiple type of question based on entity range done: more will be added
    - Testing on nmt Model done with BLEU > 95

--- gsoc/anand/LCQuAD/alpha/synonym.py
+++
@@ -1,9 +0,0 @@
-from nltk.corpus import wordnet
-import nltk 
-nltk.download('wordnet')
-synonyms = []
-
-for syn in wordnet.synsets("people"):
-    for lm in syn.lemmas():
-             synonyms.append(lm.name())
-print (set(synonyms))
