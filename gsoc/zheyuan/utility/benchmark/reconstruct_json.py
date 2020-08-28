c9fa5f1c53ffbe3f18a9a4ebb04bc945036ce115
BaiBlanc <1458491606@qq.com>
2020-08-28 23:20:50 +0200

    Benchmark modifications

--- gsoc/zheyuan/utility/benchmark/reconstruct_json.py
+++ gsoc/zheyuan/utility/benchmark/reconstruct_json.py
@@ -2,9 +2,11 @@ def construct_json(dataset_id,infos, questions, sparqls, answers):
     qald_test_answers_dict = {}
     qald_test_answers_dict["dataset"] = {'id':dataset_id}
     qald_test_answers_dict['questions'] = []
+    print(len(answers))
     for index,info in enumerate(infos):
 
         question_dict = info
+
         id = info["id"]
         question = questions[id]
         question_dict["question"] = [{
@@ -13,6 +15,7 @@ def construct_json(dataset_id,infos, questions, sparqls, answers):
         }]
         question_dict["query"] = {"sparql" : sparqls[index]}
         question_dict["answers"] = answers[index]
+        print(answers[index])
         qald_test_answers_dict['questions'].append(question_dict)
     return qald_test_answers_dict
 
