66adabd4ac8178b3973930f692475748f075a47a
BaiBlanc <1458491606@qq.com>
2020-08-28 23:20:31 +0200

    Benchmark modifications

--- gsoc/zheyuan/utility/benchmark/interpreter.py
+++ gsoc/zheyuan/utility/benchmark/interpreter.py
@@ -5,16 +5,17 @@ def interprete(trained_model_folder):
     os.system('pwd')
     print('start')
     folder_name = 'data/'+trained_model_folder
+    print('python -m nmt.nmt  --vocab_prefix=../' + folder_name + '/vocab --out_dir=../' + folder_name + '_model  --inference_input_file=../gsoc/zheyuan/utility/benchmark/to_ask1.txt  --inference_output_file=../gsoc/zheyuan/utility/benchmark/output1.txt --src=en --tgt=sparql | tail -n4')
     os.system(
-        'python -m nmt.nmt  --vocab_prefix=../' + folder_name + '/vocab --model_dir=../' + folder_name + '_model  --inference_input_file=../gsoc/zheyuan/utility/benchmark/to_ask.txt  --inference_output_file=../gsoc/zheyuan/utility/benchmark/output.txt --out_dir=../' + folder_name + '_model --src=en --tgt=sparql | tail -n4')
+        'python -m nmt.nmt  --vocab_prefix=../' + folder_name + '/vocab --out_dir=../' + folder_name + '_model  --inference_input_file=../gsoc/zheyuan/utility/benchmark/to_ask1.txt  --inference_output_file=../gsoc/zheyuan/utility/benchmark/output1.txt --src=en --tgt=sparql | tail -n4')
 
     os.system('''if [ $? -eq 0 ]
             then
                 echo ""
                 echo "ANSWER IN SPARQL SEQUENCE:"
-                ENCODED="$(cat ../gsoc/zheyuan/utility/benchmark/output.txt)"
-                python ../interpreter.py "${ENCODED}" > ../gsoc/zheyuan/utility/benchmark/output_decoded.txt
-                cat ../gsoc/zheyuan/utility/benchmark/output_decoded.txt
+                ENCODED="$(cat ../gsoc/zheyuan/utility/benchmark/output1.txt)"
+                python ../interpreter.py "${ENCODED}" > ../gsoc/zheyuan/utility/benchmark/output_decoded1.txt
+                cat ../gsoc/zheyuan/utility/benchmark/output_decoded1.txt
                 echo ""
             fi''')
     print('end')
@@ -25,3 +26,4 @@ if __name__ == "__main__":
     """
     interprete('monument_300')
     pass
+
